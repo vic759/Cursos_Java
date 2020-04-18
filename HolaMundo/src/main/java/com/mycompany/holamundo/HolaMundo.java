d4f12bf17c35654628d4f81e07b2973630b53edc
Victor Gutierrez <vic7592@gmail.com>
2020-04-18 00:32:48 -0500

    Creación poryecto HolaMundo

---
+++ HolaMundo/src/main/java/com/mycompany/holamundo/HolaMundo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.holamundo;
+
+/**
+ *
+ * @author vic759
+ */
+public class HolaMundo {
+    
+    public static void main(String args[]) {
+        System.out.println("Hola Mundo");
+    }
+}
